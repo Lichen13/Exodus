825ccdd65d443200002e10612c179eaaf8a33751
Lichen <161976633+Lichen13@users.noreply.github.com>
2025-02-17 09:38:05 -0500

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,1 +1,48 @@
+import time
+import threading
+import subprocess
+##################################
+class load:
+   def __init__(self):
+      self.endLoadingScreen = False
 
+      exec('subprocess.run(["clear"]); time.sleep(1)')
+
+      thread1 = threading.Thread(target=self.text)
+      thread2 = threading.Thread(target=self.wheel)
+      thread3 = threading.Thread(target=self.do)
+      thread1.start()
+      thread2.start()
+      thread3.start()
+      thread1.join()
+      thread2.join()
+      thread3.join()
+
+      if self.endLoadingScreen == True:
+         subprocess.run(["clear"])
+         print("[|] Starting Exodus, please wait...")
+         time.sleep(1)
+         print(" ____ __  _ ___  ___  __ __ ___")
+         print("|  __|\ \/ /   \|   \|  |  / __|")
+         print("|  _|  |  |  |  | |  |  |  \__ \\")
+         print("|____|/_/\_\___/|___/ \___/|___/")
+         print("+ ---------------------------- +")
+         print("|   By:        Lichen          |")
+         print("|   Version:     Beta          |")
+         print("+ ---------------------------- +")
+##################################
+   def wheel(self):
+         char = ["[|]", "[/]", "[-]", "[\\]"]
+         while self.endLoadingScreen == False:
+            for i in char:
+               print(i, end="\r")
+               time.sleep(0.3)
+##################################
+   def text(self):
+      print("    Starting Exodus, please wait...", end="\r")
+##################################
+   def do(self):
+      pass
+      self.endLoadingScreen = True
+
+load()
