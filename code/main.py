d04f13347e8c955b0d281f2b78a6e15a1f4fb416
Lichen <161976633+Lichen13@users.noreply.github.com>
2025-02-18 09:03:34 -0500

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,57 +1,71 @@
 import time
-import threading
 import subprocess
-import platform
-import os
 import sys
-##################################
-class load:
+import platform
+import threading
+####################################
+class loadingScreen:
+   ####################################
    def __init__(self):
-      if platform.system() == "Linux":
-         pass
-      else:
-         print("Exodus only works on Linux.")
-         sys.exit(1)
+      self.loadLogo = False
+      self.connected = False
 
-      self.endLoadingScreen = False
+      thread1 = threading.Thread(target=self.loadingText)
+      thread2 = threading.Thread(target=self.do_task)
 
-      thread1 = threading.Thread(target=self.text)
-      thread2 = threading.Thread(target=self.wheel)
-      thread3 = threading.Thread(target=self.do)
       thread1.start()
       thread2.start()
-      thread3.start()
+
       thread1.join()
       thread2.join()
-      thread3.join()
 
       self.logo()
-   ##################################
-   def wheel(self):
-         char = ["[|]", "[/]", "[-]", "[\\]"]
-         while self.endLoadingScreen == False:
-            for i in char:
-               print(i, end="\r")
-               time.sleep(0.3)
-   ##################################
-   def text(self):
-      print("    Starting Exodus, please wait...", end="\r")
-   ##################################
+   ####################################
+   def loadingText(self):
+      while self.loadLogo != True:
+         subprocess.run(["clear"])
+         print("")
+         text = ["[|] Loading Exodus, please be patient...", "[/] Loading Exodus, please be patient...", "[-] Loading Exodus, please be patient...", "[\\] Loading Exodus, please be patient..."]
+         for i in text:
+            print(i, end="\r", flush=True)
+            time.sleep(1)
+   ####################################
+   def reportError(self, description):
+      print(f"\n ➥  An error occured: '{description}'.")
+      sys.exit(1)
+   ####################################
    def logo(self):
-      if self.endLoadingScreen == True:
-         print("[|] Starting Exodus, please wait..."); time.sleep(1)
-         print(" ____ __  _ ___  ___  __ __ ___")
-         print("|  __|\ \/ /   \|   \|  |  / __|")
-         print("|  _|  |  |  |  | |  |  |  \__ \\")
-         print("|____|/_/\_\___/|___/ \___/|___/")
-         print("+ ---------------------------- +")
-         print("|   By:        Lichen          |")
-         print("|   Version:     Beta          |")
-         print("+ ---------------------------- +")
-   ##################################
-   def do(self):
-      self.endLoadingScreen = True
-##################################
+       self.line1 = "| By: Lichen                   |"
+       self.line2 = "| Version: Beta                |"
+       if self.connected == True:
+          self.line3 = "| Ping: Positive               |"
+       else:
+          self.line3 = "| Ping: Negative               |"
+       if self.loadLogo == True:
+          print("[|] Loading Exodus, please be patient...")
+          print(" ____ __  _ ___  ___  __ __ ___")
+          print("|  __|\ \/ /   \|   \|  |  / __|")
+          print("|  _|  |  |  |  | |  |  |  \__ \\")
+          print("|____|/_/\_\___/|___/ \___/|___/")
+          print("+ ---------------------------- +")
+          print(self.line1)
+          print(self.line2)
+          print(self.line3)
+          print("+ ---------------------------- +")
+   ####################################
+   def do_task(self):
+      time.sleep(1)
+      osname = platform.system()
+      if osname == "Linu":
+         pass
+      else:
+         self.reportError("Exodus is only working for Linux")
+      try:
+         result = subprocess.run(["ping", "-c", "1", "google.com"], timeout=5, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+         self.connected = True
+         self.loadLogo = True
+      except subprocess.TimeoutExpired as e:
+         self.connected = False
+         self.loadLogo = True
 
-
-load()
+loadingScreen()
